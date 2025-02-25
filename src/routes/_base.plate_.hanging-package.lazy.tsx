25cf2149e07172ec2ec77d02ef3ce33f10b5ae0b
Bruce <recall4056@gmail.com>
2025-02-26 01:54:56 +0800

    fix: :bug: #2987

--- src/routes/_base.plate_.hanging-package.lazy.tsx
+++ src/routes/_base.plate_.hanging-package.lazy.tsx
@@ -21,7 +21,10 @@ function Page() {
   return (
     <>
       <div className="col-span-12 row-span-1 p-2">
-        <TitleRow right={<ClockPanel />} />
+        <TitleRow
+          left={<div className="text-2xl">TATA木门二十五厂</div>}
+          right={<ClockPanel />}
+        />
       </div>
 
       <BlockContainer className="col-span-12 row-span-1">
--- src/routes/_base.quality_.quality.lazy.tsx
+++ src/routes/_base.quality_.quality.lazy.tsx
@@ -19,8 +19,12 @@ function Page() {
   return (
     <>
       <div className="col-span-12 row-span-1 p-2">
-        <TitleRow right={<ClockPanel />} />
+        <TitleRow
+          left={<div className="text-2xl">TATA木门二十五厂</div>}
+          right={<ClockPanel />}
+        />
       </div>
+
       <BlockContainer className="col-span-12 row-span-1">
         <StatisticsRow />
       </BlockContainer>
