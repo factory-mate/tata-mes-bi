5ef2e53014913993fd1c87a84113a4e5e156f536
Bruce <recall4056@gmail.com>
2025-03-20 17:06:54 +0800

    fix: :bug: screen height display error on TV deivce

--- src/features/layouts/BaseLayout.tsx
+++ src/features/layouts/BaseLayout.tsx
@@ -2,9 +2,10 @@ import type { PropsWithChildren } from 'react'
 
 export default function BaseLayout(props: PropsWithChildren) {
   const { children } = props
+
   return (
     <div
-      className="grid h-screen w-screen grid-cols-12 grid-rows-12 gap-2 overflow-y-auto overflow-x-hidden bg-cover bg-no-repeat p-2 text-white transition-all"
+      className="grid h-[calc(100vh-1rem)] grid-cols-12 grid-rows-12 gap-2 overflow-y-auto overflow-x-hidden bg-cover bg-no-repeat p-2 text-white transition-all"
       style={{
         backgroundImage:
           'radial-gradient(ellipse farthest-corner at center center, #011974 100%, #020f3a 100%)'
